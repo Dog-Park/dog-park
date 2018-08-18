a66361093e45dc993ab6da72526d23ae2dca2e02
faiazrahman <42232624+faiazrahman@users.noreply.github.com>
2018-08-17 17:25:30 -0700

    Updated MapView in MainView

--- MainView.js
+++ MainView.js
@@ -1,4 +1,5 @@
-@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { TopBar } from './TopBar.js';
 import { Constants } from './Constants';
 import Swiper from 'react-native-swiper';
@@ -6,12 +7,12 @@ import MapView from 'react-native-maps';
 
 export class MainView extends React.Component {
   render() {
-@ -12,12 +13,28 @@ export class MainView extends React.Component {
+    return (
+      <View style={styles.talkBubbleSquare} >
+        <Swiper
           loop={false}
           showsPagination={false}
           index={1} >
-          <View style={styles.content}>
-            <Text style={styles.text}>"Home"</Text>
 
           <View style={styles.contentHome}>
             <Text style={styles.titleText}>Home</Text>
@@ -22,7 +23,6 @@ export class MainView extends React.Component {
           </View>
 
           <View style={styles.content}>
-            <Text style={styles.text}>"Navigation"</Text>
             <Text style={styles.text}>"Welcome to the Planetarium Parking Lot!"</Text>
             <MapView
               style={styles.map}
@@ -38,7 +38,28 @@ export class MainView extends React.Component {
           <View style={styles.content}>
             <Text style={styles.text}>"Settings"</Text>
           </View>
-@ -46,5 +63,35 @@ const styles = StyleSheet.create({
+        </Swiper>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  talkBubbleSquare: {
+    //alignSelf: 'stretch',
+    flex: 1,
+    //backgroundColor: Constants.MAIN_COLOR,
+    //borderRadius: Constants.BORDER_EDGES,
+    //marginTop: 15,
+    //marginBottom: 15
+  },
+  content: {
+    margin: 20,
+    backgroundColor: Constants.MAIN_COLOR,
+    borderRadius: Constants.BORDER_EDGES,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   text: {
     color: 'white'
@@ -73,5 +94,4 @@ export class MainView extends React.Component {
     alignItems: 'flex-start',
     justifyContent: 'flex-start'
   }
-});
 });
